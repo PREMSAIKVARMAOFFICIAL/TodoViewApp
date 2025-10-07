0c12dc29362d72b6dd0159a2d106ee41de3f34bf
Prem Sai K Varma <premsaikvarma@outlook.com>
2025-10-07 10:55:36 +0530

    Initial Commit

---
+++ TodoView/TodoViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoViewApp.swift
+//  TodoView
+//
+//  Created by Prem Sai K Varma on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
